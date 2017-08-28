201baf3fca45a1208edca5e14046eb1638bd29cb
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2017-08-28 16:44:02 +0200

    Fix the Blue edges

--- Week 2/image_filter_part_three.js
+++ Week 2/image_filter_part_three.js
@@ -10,14 +10,10 @@ var img = new SimpleImage('duke_blue_devil.png');
 print(img);     // Print original image
 
 function swapBlueGreen(img) {
-    var redCor = 0;     // Red value of the color 'Blue'
-    var greenCor = 51;  // Green value of the color 'Blue'
-    var blueCor = 227;  // Blue value of the color 'Blue'
-
+    var bluePixel;
     for(var pixel of img.values()) {
-        // Blue pixels have a greater Blue value than the Green and Red
-        // So if the blue value is greater than Red plus Green then it must be a Blue pixel
-        if(pixel.getBlue() > pixel.getRed() + pixel.getGreen()) {
+        bluePixel = pixel.getBlue();
+        if(bluePixel < 255) {
             pixel.setRed(255);
             pixel.setGreen(255);
             pixel.setBlue(0);
